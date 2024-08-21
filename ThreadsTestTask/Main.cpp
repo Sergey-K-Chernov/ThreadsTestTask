67c1a0c4129b6158e4c6f8dbc246c7e098a0d563
Sergey-K-Chernov <sergey.k.chernov@yandex.ru>
2024-08-22 00:19:57 +0300

    Add generator skeleton

--- ThreadsTestTask/Main.cpp
+++ ThreadsTestTask/Main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <thread>
+#include <random>
+
 
 
 void delay_sleep(const unsigned int milliseconds)
@@ -7,7 +9,8 @@ void delay_sleep(const unsigned int milliseconds)
 	std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
 }
 
-// ѕроще было написать
+
+// ѕроще было написать, чем уточн€ть, какой вариант ожидани€ требуетс€ в задании
 void delay_busy_loop(const unsigned int milliseconds)
 {
 	auto start = std::chrono::high_resolution_clock::now();
@@ -16,6 +19,7 @@ void delay_busy_loop(const unsigned int milliseconds)
 	}
 }
 
+
 void delay(const unsigned int milliseconds)
 {
 	delay_sleep(milliseconds);
@@ -23,7 +27,55 @@ void delay(const unsigned int milliseconds)
 }
 
 
+class Generator
+{
+public:
+	Generator();
+	~Generator();
+
+	void work();
+	//Geopoint generate();
+
+private:
+	const unsigned int GENERATION_PERIOD_MIN = 200;
+	const unsigned int GENERATION_PERIOD_MAX = 500;
+
+	std::mt19937 random_generator;
+	std::uniform_int_distribution<> distribution;
+};
+
+Generator::Generator()
+{
+	std::random_device rd;
+	random_generator = std::mt19937(rd());
+	distribution = std::uniform_int_distribution<>(GENERATION_PERIOD_MIN, GENERATION_PERIOD_MAX);
+}
+
+Generator::~Generator()
+{
+}
+
+
+void Generator::work()
+{
+	while (true)
+	{
+		int pause_duration = distribution(random_generator);
+		// auto point = generate;
+		std::cout << "Generator: " << pause_duration << '\n';
+		delay(pause_duration);
+	}
+}
+
+//Geopoint Generator::generate()
+//{
+//}
+
+
 int main()
 {
+	Generator generator;
+
+	generator.work();
 	return 0;
 }
