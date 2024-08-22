875d36e3c7a4be2199229b9bb95af53b56277444
Sergey-K-Chernov <sergey.k.chernov@yandex.ru>
2024-08-22 22:11:11 +0300

    Add points queue

---
+++ ThreadsTestTask/PointsQueue.cpp
@@ -0,0 +1,25 @@
+#include "PointsQueue.h"
+
+const std::optional<PointPair> PointsQueue::get() const
+{
+	if (queue.empty())
+	{
+		return std::nullopt;
+	}
+	std::shared_lock<std::shared_mutex> lock(mutex);
+	return queue.front();
+}
+
+
+void PointsQueue::push(PointPair&& data)
+{
+	std::unique_lock<std::shared_mutex> lock(mutex);
+	queue.push(data);
+}
+
+
+void PointsQueue::pop()
+{
+	std::unique_lock<std::shared_mutex> lock(mutex);
+	queue.pop();
+}
---
+++ ThreadsTestTask/PointsQueue.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <queue>
+#include <shared_mutex>
+#include <optional>
+
+#include "Geopoint.h"
+
+typedef std::pair<Geopoint, Geopoint> PointPair;
+
+class PointsQueue
+{
+public:
+	PointsQueue() {}
+	~PointsQueue() {}
+
+	const std::optional<PointPair> get() const;
+	void push(PointPair&& data);
+	void pop();
+
+private:
+	std::queue<PointPair, std::deque<PointPair>> queue;
+
+	mutable std::shared_mutex mutex;
+};
